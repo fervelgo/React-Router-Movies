ed2bc332f13b013e54a9c19419f0647b6ce5776c
Fernando Velasco <fervelgo@gmail.com>
2021-05-17 18:01:30 -0500

    imported needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router} from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
 
